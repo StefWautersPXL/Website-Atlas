6a0555a209950fb76b850c435c54b996bafe9074
Stef Wauters <Stef.Wauters@STUDENT.PXL.BE>
2026-04-22 11:42:44 +0200

    Update BulkManage.ps1

--- BulkManage.ps1
+++ BulkManage.ps1
@@ -1,17 +1,3 @@
-param (
-    [string]$PcPrefix,
-    [int]$StartNum,
-    [int]$EndNum,
-    [string]$Command
-)
-
-# Genereer de lijst met PC-namen
-$ComputerList = $StartNum..$EndNum | ForEach-Object { "$PcPrefix$("{0:D2}" -f $_)" }
-
-Write-Host "Verbinding maken met: $($ComputerList -join ', ')" -ForegroundColor Cyan
-
-# Voer het commando uit op de PC's
-Invoke-Command -ComputerName $ComputerList -ScriptBlock {
-    param($cmd)
-    Invoke-Expression $cmd
-} -ArgumentList $Command
+USOClient.exe StartInteractiveScan
+USOClient.exe StartDownload
+USOClient.exe StartInstall
